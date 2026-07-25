e3656d4ea866e80d3afccea21dc422c454fe0c58
Gemah Ghozali <gemahghozali@gmail.com>
2026-07-25 14:21:17 +0800

    feat : add auth login and logout custom hooks

---
+++ src/features/auth/hooks.ts
@@ -0,0 +1,69 @@
+"use client";
+
+import { toast } from "@/components/ui/toast";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { useMutation } from "@tanstack/react-query";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { login, logout } from "./actions";
+import { ActionResponse } from "@/types";
+import { LoginSchema, LoginInput } from "./schemas";
+
+export function useLogin() {
+  const router = useRouter();
+
+  const form = useForm<LoginInput>({
+    resolver: zodResolver(LoginSchema),
+    mode: "onTouched",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const { mutate, isPending, data } = useMutation({
+    mutationFn: async (data: LoginInput) => {
+      const response = await login(data);
+      if (!response.success) throw response;
+      return response;
+    },
+    onSuccess: (response) => {
+      toast.add({ type: "success", description: response.message });
+      router.replace("/dashboard");
+    },
+    onError: (response: ActionResponse) => {
+      toast.add({ type: "error", description: response.message });
+
+      if (response.errors) {
+        Object.entries(response.errors).forEach(([field, message]) => {
+          form.setError(field as keyof LoginInput, { type: "server", message });
+        });
+      }
+    },
+  });
+
+  const onSubmit = form.handleSubmit((data) => mutate(data));
+
+  return { onSubmit, form, isPending, result: data };
+}
+
+export function useLogout() {
+  const router = useRouter();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async () => {
+      const response = await logout();
+      if (!response.success) throw response;
+      return response;
+    },
+    onSuccess: (response) => {
+      toast.add({ type: "success", description: response.message });
+      router.replace("/login");
+    },
+    onError: (response: ActionResponse) => {
+      toast.add({ type: "error", description: response.message });
+    },
+  });
+
+  return { logout: mutate, isPending };
+}
