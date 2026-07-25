db8da7a50270f42d4fab5f2353f2029c0a9bb0c6
Gemah Ghozali <gemahghozali@gmail.com>
2026-07-25 23:28:21 +0800

    refactor : change tanstack query provider from 'QueryProvider' into 'QueryClientProvider'

--- src/providers/query-client.tsx
+++ src/providers/query-client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ReactNode } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider as TanstackQueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -11,6 +11,6 @@ const queryClient = new QueryClient({
   },
 });
 
-export function QueryProvider({ children }: { children: ReactNode }) {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+export function QueryClientProvider({ children }: { children: ReactNode }) {
+  return <TanstackQueryClientProvider client={queryClient}>{children}</TanstackQueryClientProvider>;
 }
