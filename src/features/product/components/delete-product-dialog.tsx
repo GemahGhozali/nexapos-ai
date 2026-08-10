0aab4815410bc172b0e7751e576f79963653c6c1
Gemah Ghozali <gemahghozali@gmail.com>
2026-08-10 18:41:46 +0800

    feat : add delete product dialog component

---
+++ src/features/product/components/delete-product-dialog.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button";
+import { Product } from "../types";
+import { Spinner } from "@/components/ui/spinner";
+import { useDeleteProduct } from "../hooks";
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+
+interface DeleteProductDialogProps {
+  product: Product | null;
+  onClose: () => void;
+}
+
+export function DeleteProductDialog({ product, onClose }: DeleteProductDialogProps) {
+  const { mutateAsync: deleteProduct, isPending } = useDeleteProduct();
+
+  const handleDelete = async () => {
+    if (!product) return;
+    await deleteProduct(product.id);
+    onClose();
+  };
+
+  return (
+    <Dialog
+      open={Boolean(product)}
+      onOpenChange={(open) => {
+        if (!open) {
+          onClose();
+        }
+      }}
+    >
+      <DialogContent className="sm:max-w-sm">
+        <DialogHeader>
+          <DialogTitle>Are you sure?</DialogTitle>
+          <DialogDescription>This product will be deleted permanently!</DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <DialogClose
+            render={
+              <Button variant="outline" onClick={onClose}>
+                Cancel
+              </Button>
+            }
+          />
+          <Button variant="destructive" type="button" disabled={isPending} onClick={handleDelete}>
+            {isPending ? "Processing" : "Delete Product"}
+            {isPending && <Spinner data-icon="inline-start" />}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
