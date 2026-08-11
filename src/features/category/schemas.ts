cd2e880d9e51f2e14c390f6c8ae6fcbcc3541b14
Gemah Ghozali <gemahghozali@gmail.com>
2026-08-11 15:04:18 +0800

    refactor : remove nullable and optional from category schema

--- src/features/category/schemas.ts
+++ src/features/category/schemas.ts
@@ -2,7 +2,7 @@ import * as z from "zod";
 
 export const CategorySchema = z.object({
   name: z.string().min(1, "Nama kategori wajib diisi!"),
-  description: z.string().nullable().optional(),
+  description: z.string(),
 });
 
 export type CategoryInput = z.infer<typeof CategorySchema>;
