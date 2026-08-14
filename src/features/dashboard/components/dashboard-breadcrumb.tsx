25537de1e4061fc91ea1e6a3d97b3f3b7265b0f8
Gemah Ghozali <gemahghozali@gmail.com>
2026-08-14 18:58:23 +0800

    feat : add dashboard breadcrumb component and put it in dashboard header component

---
+++ src/features/dashboard/components/dashboard-breadcrumb.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Link from "next/link";
+import { Fragment } from "react";
+import { segmentMapping } from "../constants";
+import { useSelectedLayoutSegments, useParams } from "next/navigation";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+
+export function DashboardBreadcrumb() {
+  const params = useParams();
+  const segments = useSelectedLayoutSegments();
+
+  const dynamicValues = Object.values(params).flat();
+  const breadcrumbItems = segments.filter((segment) => !dynamicValues.includes(segment));
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink render={<Link href="/dashboard" />}>Dashboard</BreadcrumbLink>
+        </BreadcrumbItem>
+
+        {breadcrumbItems.length > 0 && <BreadcrumbSeparator />}
+
+        {breadcrumbItems.map((segment, index) => {
+          const isNotLast = index !== breadcrumbItems.length - 1;
+
+          return (
+            <Fragment key={index}>
+              <BreadcrumbItem className="capitalize">
+                {isNotLast ? (
+                  <BreadcrumbLink render={<Link href={`/dashboard/${segment}`} />}>{segmentMapping[segment]}</BreadcrumbLink>
+                ) : (
+                  <BreadcrumbPage>{segmentMapping[segment]}</BreadcrumbPage>
+                )}
+              </BreadcrumbItem>
+              {isNotLast && <BreadcrumbSeparator />}
+            </Fragment>
+          );
+        })}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
--- src/features/dashboard/components/dashboard-header.tsx
+++ src/features/dashboard/components/dashboard-header.tsx
@@ -1,11 +1,11 @@
-import { UserProfile } from "./user-profile";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { DashboardBreadcrumb } from "./dashboard-breadcrumb";
 
-export default function DashboardHeader() {
+export function DashboardHeader() {
   return (
-    <header className="w-full flex items-center justify-between p-6">
-      <SidebarTrigger />
-      <UserProfile />
+    <header className="w-full flex items-center gap-4 p-6 pb-0">
+      <SidebarTrigger size="icon-lg" />
+      <DashboardBreadcrumb />
     </header>
   );
 }
