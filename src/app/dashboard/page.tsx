0563c7a1cb27bf6c44285678a89edbc28ddf95ee
Gemah Ghozali <gemahghozali@gmail.com>
2026-08-14 19:15:31 +0800

    refactor : make dashboard page into basic page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,3 @@
-import UserProfile from "@/features/dashboard/components/user-profile";
-
 export default function DashboardPage() {
-  return (
-    <div className="w-full flex items-center justify-between p-4">
-      <p>Dashboard Page </p>
-      <UserProfile />
-    </div>
-  );
+  return <div>Dashboard Page</div>;
 }
