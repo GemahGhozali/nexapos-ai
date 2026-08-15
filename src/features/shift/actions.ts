be669d7128928a59072a787e13ad1b6b9856c7f9
Gemah Ghozali <gemahghozali@gmail.com>
2026-08-15 11:20:54 +0800

    feat : add open shift and close shift action functions

---
+++ src/features/shift/actions.ts
@@ -0,0 +1,108 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { createClient } from "@/libs/supabase/server";
+import { formatZodError } from "@/utils/format-zod-error";
+import { revalidatePath } from "next/cache";
+import { getCurrentUserAndActiveShift } from "./queries";
+import { OpeningShiftSchema, OpeningShiftInput, ClosingShiftSchema, ClosingShiftInput } from "./schemas";
+
+export async function openShift(data: OpeningShiftInput) {
+  try {
+    const supabase = await createClient();
+
+    const validated = OpeningShiftSchema.safeParse(data);
+
+    if (!validated.success) {
+      return {
+        success: false,
+        message: "Data tidak valid!",
+        errors: formatZodError(validated.error),
+      };
+    }
+
+    const { openingCash } = validated.data;
+
+    const { user, shift } = await getCurrentUserAndActiveShift();
+
+    if (!user) redirect("/login");
+
+    if (shift) return { success: false, message: "Shift anda masih berstatus aktif!" };
+
+    const { error } = await supabase.from("shifts").insert({ user_id: user.id, opening_cash: openingCash, status: "open" });
+
+    if (error) {
+      console.log("❌ Open Shift Error:", error);
+      return { success: false, message: "Gagal membuka shift!" };
+    }
+
+    revalidatePath("/dashboard", "layout");
+
+    return { success: true, message: "Shift berhasil dibuka!" };
+  } catch (error) {
+    console.log("❌ Open Shift Error:", error);
+    return { success: false, message: "Terjadi kesalahan pada server!" };
+  }
+}
+
+export async function closeShift(data: ClosingShiftInput) {
+  try {
+    const supabase = await createClient();
+
+    const validated = ClosingShiftSchema.safeParse(data);
+
+    if (!validated.success) {
+      return {
+        success: false,
+        message: "Data tidak valid!",
+        errors: formatZodError(validated.error),
+      };
+    }
+
+    const { closingCash } = validated.data;
+
+    const { user, shift } = await getCurrentUserAndActiveShift();
+
+    if (!user) redirect("/login");
+
+    if (!shift) return { success: false, message: "Shift yang ingin ditutup tidak ditemukan!" };
+
+    const { data: totalCashTransactions, error: getTotalCashTransactionsError } = await supabase
+      .from("transactions")
+      .select("total_amount")
+      .eq("shift_id", shift.id)
+      .eq("payment_method", "cash");
+
+    if (getTotalCashTransactionsError) {
+      console.log("❌ Close Shift Error:", getTotalCashTransactionsError);
+      return { success: false, message: "Gagal menutup shift!" };
+    }
+
+    const totalCashIn = totalCashTransactions.reduce((total, cash) => total + cash.total_amount, 0);
+    const expectedCash = shift.openingCash + totalCashIn;
+    const cashDifference = closingCash - expectedCash;
+
+    const { error: closingShiftError } = await supabase
+      .from("shifts")
+      .update({
+        closing_cash: closingCash,
+        expected_cash: expectedCash,
+        cash_difference: cashDifference,
+        status: "closed",
+        closed_at: new Date().toISOString(),
+      })
+      .eq("id", shift.id);
+
+    if (closingShiftError) {
+      console.log("❌ Close Shift Error:", closingShiftError);
+      return { success: false, message: "Gagal menutup shift!" };
+    }
+
+    revalidatePath("/dashboard", "layout");
+
+    return { success: true, message: "Shift berhasil ditutup!" };
+  } catch (error) {
+    console.log("❌ Close Shift Error:", error);
+    return { success: false, message: "Terjadi kesalahan pada server!" };
+  }
+}
