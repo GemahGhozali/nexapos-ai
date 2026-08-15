45f01eafb6364b73cb2c04fc21bbfbc2b3eeb952
Gemah Ghozali <gemahghozali@gmail.com>
2026-08-15 11:29:20 +0800

    refactor : change dashboard page by adding close shift form dialog component and add more typography in it

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,13 @@
+import { CloseShiftFormDialog } from "@/features/shift/components/close-shift-form-dialog";
+
 export default function DashboardPage() {
-  return <div>Dashboard Page</div>;
+  return (
+    <div className="flex justify-between items-end gap-6">
+      <div className="space-y-1">
+        <p className="font-heading font-semibold">Shift Overview</p>
+        <p className="text-sm text-muted-foreground">Pantau ringkasan penjualan dan aktivitas kas selama shift aktif</p>
+      </div>
+      <CloseShiftFormDialog />
+    </div>
+  );
 }
