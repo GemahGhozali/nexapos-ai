af4da3e0c7d1b33e0b9e7afcc09ece004cb20c12
Gemah Ghozali <gemahghozali@gmail.com>
2026-08-16 12:30:41 +0800

    feat : add cashflow form component and integrate it in cashflow mutation and operational cashflow page

---
+++ src/app/dashboard/(admin)/cashflow-mutation/create/page.tsx
@@ -0,0 +1,5 @@
+import { CashflowForm } from "@/features/cashflow/components/cashflow-form";
+
+export default function CreateCashflowMutationPage() {
+  return <CashflowForm insertDataIntoActiveShift={false} />;
+}
---
+++ src/app/dashboard/(operational)/operational-cashflow/create/page.tsx
@@ -0,0 +1,5 @@
+import { CashflowForm } from "@/features/cashflow/components/cashflow-form";
+
+export default function CreateOperationalCashflowPage() {
+  return <CashflowForm insertDataIntoActiveShift={true} />;
+}
