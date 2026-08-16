70ab29c3854f43e65b72e8d04dae8cadaaef52d5
Gemah Ghozali <gemahghozali@gmail.com>
2026-08-16 12:29:26 +0800

    feat : add constants for cashflow category

---
+++ src/features/cashflow/constants.ts
@@ -0,0 +1,4 @@
+export const CASHFLOW_CATEGORIES = {
+  income: ["Suntikan Dana", "Pendapatan Non-Operasional", "Lainnya"],
+  expense: ["Biaya Produksi", "Biaya Operasional", "Biaya Bahan Baku", "Biaya Pemasaran", "Biaya Pemeliharaan", "Biaya Tenaga Kerja", "Lainnya"],
+};
