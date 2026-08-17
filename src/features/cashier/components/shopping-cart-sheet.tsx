f89adec6dfcb950a4277e121e7bafc6ca6c471b1
Gemah Ghozali <gemahghozali@gmail.com>
2026-08-17 12:26:58 +0800

    refactor : update shopping cart sheet component. add internal open state, add transaction summary and integrate checkout form sheet component in it

--- src/features/cashier/components/shopping-cart-sheet.tsx
+++ src/features/cashier/components/shopping-cart-sheet.tsx
@@ -1,16 +1,23 @@
+"use client";
+
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Separator } from "@/components/ui/separator";
 import { formatToIDR } from "@/utils/format-to-idr";
 import { useCartStore } from "../stores";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { ShoppingCartItem } from "./shopping-cart-item";
-import { Cancel01Icon, ShoppingCart01Icon } from "@hugeicons/core-free-icons";
+import { CheckoutFormSheet } from "./checkout-form-sheet";
+import { Cancel01Icon, Money04Icon, ShoppingCart01Icon } from "@hugeicons/core-free-icons";
 import { Sheet, SheetClose, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 
 export function CartSheet() {
   const cart = useCartStore((state) => state.cart);
-  const itemQuantity = useCartStore((state) => state.cart.reduce((total, item) => total + item.quantity, 0));
-  const totalPrice = useCartStore((state) => state.cart.reduce((total, item) => total + item.price * item.quantity, 0));
+  const itemQuantity = cart.reduce((total, item) => total + item.quantity, 0);
+  const totalAmount = cart.reduce((total, item) => total + item.price * item.quantity, 0);
+
+  const [open, setOpen] = useState<boolean>(false);
 
   const renderItems = () => {
     if (itemQuantity === 0) {
@@ -35,7 +42,7 @@ export function CartSheet() {
   };
 
   return (
-    <Sheet>
+    <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger
         render={
           <Button size="icon-lg" variant={itemQuantity === 0 ? "secondary" : "default"} className="fixed bottom-6 right-6 z-50">
@@ -57,8 +64,27 @@ export function CartSheet() {
         </SheetHeader>
         {renderItems()}
         {itemQuantity > 0 && (
-          <SheetFooter className="border-t p-4 space-y-4">
-            <Button>Checkout - {formatToIDR(totalPrice)}</Button>
+          <SheetFooter className="border-t p-4 gap-4">
+            <div className="p-3 space-y-3 border rounded-lg bg-secondary">
+              <p className="font-semibold text-base">Ringkasan Transaksi</p>
+              <Separator />
+              <div className="flex justify-between gap-3">
+                <p className="text-muted-foreground flex items-center gap-2">
+                  <HugeiconsIcon icon={ShoppingCart01Icon} size={16} strokeWidth={2} color="currentColor" />
+                  Total Item
+                </p>
+                <p className="font-medium">{itemQuantity} Item</p>
+              </div>
+              <div className="flex justify-between gap-3">
+                <p className="text-muted-foreground flex items-center gap-2">
+                  <HugeiconsIcon icon={Money04Icon} size={16} strokeWidth={2} color="currentColor" />
+                  Total Keseluruhan
+                </p>
+                <p className="font-medium">{formatToIDR(totalAmount)}</p>
+              </div>
+              <Separator />
+              <CheckoutFormSheet onCheckoutSuccess={() => setOpen(false)} />
+            </div>
           </SheetFooter>
         )}
       </SheetContent>
