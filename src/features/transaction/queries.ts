601b117f7972bae21dbdd7a07945562328cec26b
Gemah Ghozali <gemahghozali@gmail.com>
2026-08-18 10:26:58 +0800

    feat : add get all transactions query function and add user field in all transaction data

--- src/features/transaction/queries.ts
+++ src/features/transaction/queries.ts
@@ -6,6 +6,54 @@ import { createClient } from "@/libs/supabase/server";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { getCurrentUserAndActiveShift } from "../shift/queries";
 
+export async function getAllTransactions() {
+  try {
+    const supabase = await createClient();
+
+    const { data, error } = await supabase
+      .from("transactions")
+      .select(
+        `
+        id,
+        date,
+        user: profiles(fullname),
+        totalAmount: total_amount,
+        paymentMethod: payment_method,
+        paidAmount: paid_amount,
+        changeAmount: change_amount,
+        items: transaction_items (id, productName: product_name, image: products(image), priceAtSale: price_at_sale, quantity, subtotal)
+      `,
+      )
+      .order("date", { ascending: false });
+
+    if (error) {
+      console.error("❌ Get All Shift Transactions Error :", error);
+      return { data: null, error: "Gagal mengambil data transaksi!" };
+    }
+
+    return {
+      error: null,
+      data: data.map((transaction) => {
+        const user = Array.isArray(transaction.user) ? transaction.user[0] : transaction.user;
+
+        return {
+          ...transaction,
+          user,
+          items: transaction.items.map((item) => {
+            const product = Array.isArray(item.image) ? item.image[0] : item.image;
+            const image = product.image ? `${ENVIRONMENT.SUPABASE_STORAGE_URL}/images/${product.image}` : null;
+            return { ...item, image };
+          }),
+        };
+      }),
+    };
+  } catch (error) {
+    if (isRedirectError(error)) throw error;
+    console.log("❌ Get All Transactions Error :", error);
+    return { data: null, error: "Terjadi kesalahan pada server!" };
+  }
+}
+
 export async function getActiveShiftTransactions() {
   try {
     const supabase = await createClient();
@@ -22,6 +70,7 @@ export async function getActiveShiftTransactions() {
         `
         id,
         date,
+        user: profiles(fullname),
         totalAmount: total_amount,
         paymentMethod: payment_method,
         paidAmount: paid_amount,
@@ -39,14 +88,19 @@ export async function getActiveShiftTransactions() {
 
     return {
       error: null,
-      data: data.map((transaction) => ({
-        ...transaction,
-        items: transaction.items.map((item) => {
-          const product = Array.isArray(item.image) ? item.image[0] : item.image;
-          const image = product.image ? `${ENVIRONMENT.SUPABASE_STORAGE_URL}/images/${product.image}` : null;
-          return { ...item, image };
-        }),
-      })),
+      data: data.map((transaction) => {
+        const user = Array.isArray(transaction.user) ? transaction.user[0] : transaction.user;
+
+        return {
+          ...transaction,
+          user,
+          items: transaction.items.map((item) => {
+            const product = Array.isArray(item.image) ? item.image[0] : item.image;
+            const image = product.image ? `${ENVIRONMENT.SUPABASE_STORAGE_URL}/images/${product.image}` : null;
+            return { ...item, image };
+          }),
+        };
+      }),
     };
   } catch (error) {
     if (isRedirectError(error)) throw error;
