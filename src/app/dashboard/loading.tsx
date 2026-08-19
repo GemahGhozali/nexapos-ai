54fa7dbf22191d8d279b04fe90bf293a8aeec75f
Gemah Ghozali <gemahghozali@gmail.com>
2026-08-19 22:33:49 +0800

    refactor : integrate all component that show active shift's data and statistics in dashboard page

---
+++ src/app/dashboard/loading.tsx
@@ -0,0 +1,61 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function DashboardLoadingPage() {
+  return (
+    <div className="space-y-6">
+      <div className="w-full flex justify-between items-end gap-6">
+        <div className="grow space-y-2.5">
+          <Skeleton className="h-7 w-1/4 rounded-lg" />
+          <Skeleton className="h-3.5 w-2/4 rounded-lg" />
+        </div>
+        <Skeleton className="h-9 w-24 rounded-full" />
+      </div>
+      <div className="grid grid-cols-4 gap-6 h-[180px]">
+        {Array.from({ length: 4 }).map((_, index) => (
+          <Card key={index}>
+            <CardHeader>
+              <Skeleton className="size-12 rounded-full mb-4" />
+              <CardDescription>
+                <Skeleton className="w-2/4 h-5 rounded-lg" />
+              </CardDescription>
+              <CardTitle>
+                <Skeleton className="w-3/4 h-8 rounded-lg" />
+              </CardTitle>
+            </CardHeader>
+          </Card>
+        ))}
+      </div>
+      <div className="grid grid-cols-2 gap-6 h-[505px]">
+        {Array.from({ length: 2 }).map((_, index) => (
+          <Card key={index}>
+            <CardHeader>
+              <CardTitle>
+                <Skeleton className="w-2/4 h-8 rounded-lg" />
+              </CardTitle>
+              <CardDescription>
+                <Skeleton className="w-3/4 h-5 rounded-lg" />
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="size-full">
+              <Skeleton className="size-full" />
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+      <Card className="w-full h-[374px]">
+        <CardHeader>
+          <CardTitle>
+            <Skeleton className="w-1/4 h-8 rounded-lg" />
+          </CardTitle>
+          <CardDescription>
+            <Skeleton className="w-2/4 h-5 rounded-lg" />
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="size-full">
+          <Skeleton className="size-full" />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,41 @@
+import { getCurrentTime } from "@/utils/get-current-time";
+import { ShiftKpiStatsCards } from "@/features/dashboard/components/shift-kpi-stats-cards";
 import { CloseShiftFormDialog } from "@/features/shift/components/close-shift-form-dialog";
+import { PaymentMethodPieChart } from "@/features/dashboard/components/payment-method-pie-chart";
+import { OperationalCashTracker } from "@/features/dashboard/components/operational-cash-tracker";
+import { TransactionTrendAreaChart } from "@/features/dashboard/components/transaction-trend-area-chart";
+import { getCurrentUserAndActiveShift } from "@/features/shift/queries";
+import { getKpiStats, getCashDrawerDetails, getPaymentMethodProportion, getHourlyTransactionTrend } from "@/features/dashboard/queries";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const { user, shift } = await getCurrentUserAndActiveShift();
+
+  if (!shift) return null;
+
+  const [kpiStats, cashDrawerStats, paymentMethodPieData, hourlyTrendAreaData] = await Promise.all([
+    getKpiStats(shift.id),
+    getCashDrawerDetails(shift.id),
+    getPaymentMethodProportion(shift.id),
+    getHourlyTransactionTrend(shift.id),
+  ]);
+
   return (
-    <div className="flex justify-between items-end gap-6">
-      <div className="space-y-1">
-        <p className="font-heading font-semibold">Shift Overview</p>
-        <p className="text-sm text-muted-foreground">Pantau ringkasan penjualan dan aktivitas kas selama shift aktif</p>
+    <div className="space-y-6">
+      <div className="flex justify-between items-end gap-6">
+        <div className="space-y-1">
+          <p className="text-xl font-medium">
+            Selamat {getCurrentTime()}, {user.fullname}! 🙌
+          </p>
+          <p className="text-sm text-muted-foreground">Lihat ringkasan penjualan dan aktivitas kas selama sesi shift berlangsung.</p>
+        </div>
+        <CloseShiftFormDialog />
       </div>
-      <CloseShiftFormDialog />
+      <ShiftKpiStatsCards data={kpiStats} />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <OperationalCashTracker data={cashDrawerStats} />
+        <PaymentMethodPieChart data={paymentMethodPieData} />
+      </div>
+      <TransactionTrendAreaChart data={hourlyTrendAreaData} />
     </div>
   );
 }
