03e3a36ae2cdd0f1a40031781fec632d9eec66f2
Gemah Ghozali <gemahghozali@gmail.com>
2026-08-23 14:36:27 +0800

    feat : add ai wizard action functions for generating prompt, process wizard input and transcribe audio to text

---
+++ src/features/ai/wizard/actions.ts
@@ -0,0 +1,108 @@
+"use server";
+
+import { groq } from "@/libs/groq";
+import { AI_TOOLS } from "./constants";
+import { createClient } from "@/libs/supabase/server";
+import { AIToolKey, WizardResponse } from "./types";
+import { WizardInput, WizardSchema } from "./schemas";
+import { createAIAction, getAITools, isToolRequireProductData } from "./utils";
+
+interface ProcessWizardInputParams {
+  data: WizardInput;
+  allowedTools: AIToolKey[];
+}
+
+async function generateWizardSystemPrompt(allowedTools: AIToolKey[]) {
+  let systemPrompt = `
+    # ROLE
+    You are an AI Wizard assitant, who can extract information from text.
+    
+    # INSTRUCTION
+    Extract data from the text. Never output conversational text or explanation. Only respond with function/tool call.
+  `;
+
+  if (!isToolRequireProductData(allowedTools)) {
+    return systemPrompt;
+  }
+
+  const supabase = await createClient();
+  const { data } = await supabase.from("products").select("id, name, price, image");
+
+  if (!data) return systemPrompt;
+
+  const catalogText = data.map(({ id, name, price, image }) => `${id}:${name}:${price}:${image}`).join("|");
+
+  systemPrompt += `\n# PRODUCTS (Match user intent to product ID):\n${catalogText}`;
+
+  return systemPrompt;
+}
+
+export async function processWizardInput({ data, allowedTools }: ProcessWizardInputParams): Promise<WizardResponse> {
+  const validated = WizardSchema.safeParse(data);
+
+  if (!validated.success) {
+    return { success: false, message: "Perintah tidak valid!" };
+  }
+
+  try {
+    const tools = getAITools(allowedTools);
+    const systemPrompt = await generateWizardSystemPrompt(allowedTools);
+
+    const response = await groq.chat.completions.create({
+      model: "openai/gpt-oss-20b",
+      messages: [
+        { role: "system", content: systemPrompt },
+        { role: "user", content: validated.data.prompt },
+      ],
+      tools: tools,
+      tool_choice: "auto",
+    });
+
+    const toolCalls = response.choices[0].message.tool_calls;
+
+    if (toolCalls && toolCalls.length > 0) {
+      for (const toolCall of toolCalls) {
+        const functionName = toolCall.function.name as AIToolKey;
+        const toolConfig = AI_TOOLS[functionName];
+
+        if (!toolConfig) continue;
+
+        const functionArguments = JSON.parse(toolCall.function.arguments);
+        const toolSchema = AI_TOOLS[functionName].metadata.schema;
+        const parseResult = toolSchema.safeParse(functionArguments);
+
+        if (parseResult.success) {
+          const action = createAIAction(functionName, parseResult.data);
+          return { success: true, message: toolConfig.metadata.successMessage, data: { action } };
+        }
+      }
+    }
+
+    return { success: false, message: "AI tidak memiliki kemampuan menjalankan perintah!" };
+  } catch (error) {
+    console.log("❌ Process Wizard Input Error :", error);
+    return { success: false, message: "Terjadi kesalahan pada server!" };
+  }
+}
+
+export async function transcribeAudio(formData: FormData) {
+  try {
+    const file = formData.get("file") as File;
+
+    if (!file) {
+      return { success: false, message: "Gagal memproses perintah suara!" };
+    }
+
+    const transcription = await groq.audio.transcriptions.create({
+      file: file,
+      model: "whisper-large-v3-turbo",
+      language: "id",
+      response_format: "json",
+    });
+
+    return { success: true, message: "Berhasil memproses perintah suara!", data: transcription.text };
+  } catch (error) {
+    console.log("❌ Transcribe Audio Error :", error);
+    return { success: false, message: "Gagal memproses perintah suara!" };
+  }
+}
