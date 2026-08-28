33eb7961029a16eca3d265b26915bb370d1c6e91
Gemah Ghozali <gemahghozali@gmail.com>
2026-08-28 19:19:12 +0800

    refactor : delete cashflow feature and all its related thing

--- src/features/cashflow/constants.ts
+++
@@ -1,4 +0,0 @@
-export const CASHFLOW_CATEGORIES = {
-  income: ["Suntikan Dana", "Pendapatan Non-Operasional", "Lainnya"],
-  expense: ["Biaya Produksi", "Biaya Operasional", "Biaya Bahan Baku", "Biaya Pemasaran", "Biaya Pemeliharaan", "Biaya Tenaga Kerja", "Lainnya"],
-};
--- src/features/cashflow/types.ts
+++
@@ -1,9 +0,0 @@
-export type Cashflow = {
-  date: Date;
-  amount: number;
-  category: string;
-  type: "income" | "expense";
-  paymentMethod: "cash" | "transfer";
-  description: string;
-  user: { fullname: string } | null;
-};
